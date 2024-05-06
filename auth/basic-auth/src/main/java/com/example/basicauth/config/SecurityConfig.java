6222a3266592deaadb9cd4cbde9c7405052cbe68
RustamHuseynov02 <rustem.huseynov.2016@gmail.com>
2024-05-06 13:39:58 +0400

    session timeout added

--- auth/basic-auth/src/main/java/com/example/basicauth/config/SecurityConfig.java
+++ auth/basic-auth/src/main/java/com/example/basicauth/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.basicauth.config;
 
-import org.aspectj.asm.internal.HandleProviderDelimiter;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +7,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
@@ -43,8 +42,11 @@ public class SecurityConfig {
 				.anyRequest().authenticated()
 						)
 				
-				.formLogin(AbstractHttpConfigurer::disable)
-				.httpBasic(Customizer.withDefaults());
+				.formLogin(Customizer.withDefaults())
+				.httpBasic(Customizer.withDefaults())
+				.sessionManagement(x -> x.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
+
+
 
 		return security.build();
 	}
